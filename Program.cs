bc581a9326a5f7cb5c691119939cfed5728a96a4
hevolx <143357495+hevolx@users.noreply.github.com>
2024-10-08 06:53:33 +0200

    Update Program.cs
    I found a bug

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("I changed the codes");
+            Console.WriteLine("I changed the codes"); // There is a bug over here
             Console.ReadLine();
         }
     }
